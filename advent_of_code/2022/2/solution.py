0679d2b16d4f10d70ffc8828d19a384e769174c1
James Rine <rinejames@gmail.com>
2022-12-02 19:52:09 -0500

    update template to include values in test output

--- advent_of_code/2022/2/solution.py
+++ advent_of_code/2022/2/solution.py
@@ -11,16 +11,12 @@ PART_2_SAMPLE_INPUT = get_full_text("samples/part_2_input.txt")
 PART_2_SAMPLE_SOLUTION = get_full_text("samples/part_2_solution.txt")
 
 
-def part_1_solution_test():
-    if part_1_solution(PART_1_SAMPLE_INPUT) == PART_1_SAMPLE_SOLUTION:
+def solution_test(function, sample_input, sample_solution):
+    result = function(sample_input)
+    if result == sample_solution:
         return "PASS"
-    return "FAIL"
+    return f"FAIL: got: {result} expected: {sample_solution}"
 
-
-def part_2_solution_test():
-    if part_2_solution(PART_2_SAMPLE_INPUT) == PART_2_SAMPLE_SOLUTION:
-        return "PASS"
-    return f"FAIL {part_2_solution(PART_2_SAMPLE_INPUT)}"
 
 
 def letter_to_value(letter):
@@ -78,6 +74,12 @@ if __name__ == "__main__":
     if not INPUT:
         INPUT = download_input(*year_and_date(__file__))
         with open("input.txt", "wb") as f:
-            f.write(INPUT.encode())
-    print(f"Part_1: {part_1_solution_test()} -- Full Solution: {part_1_solution(INPUT)}")
-    print(f"Part_2: {part_2_solution_test()} -- Full Solution: {part_2_solution(INPUT)}")
+            f.write(INPUT)
+    part_1 = solution_test(part_1_solution, PART_1_SAMPLE_INPUT, PART_1_SAMPLE_SOLUTION)
+    part_2 = solution_test(part_2_solution, PART_2_SAMPLE_INPUT, PART_2_SAMPLE_SOLUTION)
+
+    print(f"Part_1: {part_1}")
+    print(f"Part_2: {part_2}")
+    print('---SOLUTIONS---')
+    print(f"Part_1: {part_1_solution(INPUT)}")
+    print(f"Part_2: {part_2_solution(INPUT)}")
--- template/solution.py
+++ template/solution.py
@@ -11,16 +11,11 @@ PART_2_SAMPLE_INPUT = get_full_text("samples/part_2_input.txt")
 PART_2_SAMPLE_SOLUTION = get_full_text("samples/part_2_solution.txt")
 
 
-def part_1_solution_test():
-    if part_1_solution(PART_1_SAMPLE_INPUT) == PART_1_SAMPLE_SOLUTION:
+def solution_test(function, sample_input, sample_solution):
+    result = function(sample_input)
+    if result == sample_solution:
         return "PASS"
-    return "FAIL"
-
-
-def part_2_solution_test():
-    if part_2_solution(PART_2_SAMPLE_INPUT) == PART_2_SAMPLE_SOLUTION:
-        return "PASS"
-    return "FAIL"
+    return f"FAIL: got: {result} expected: {sample_solution}"
 
 
 def part_1_solution(puzzle_input):
@@ -42,6 +37,11 @@ if __name__ == "__main__":
         INPUT = download_input(*year_and_date(__file__))
         with open("input.txt", "wb") as f:
             f.write(INPUT)
-    print(f"Part_1: {part_1_solution_test()} -- Full Solution: {part_1_solution(INPUT)}")
+    part_1 = solution_test(part_1_solution, PART_1_SAMPLE_INPUT, PART_1_SAMPLE_SOLUTION)
+    part_2 = solution_test(part_2_solution, PART_2_SAMPLE_INPUT, PART_2_SAMPLE_SOLUTION)
 
-    print(f"Part_2: {part_2_solution_test()} -- Full Solution: {part_2_solution(INPUT)}")
+    print(f"Part_1: {part_1}")
+    print(f"Part_2: {part_2}")
+    print('---SOLUTIONS---')
+    print(f"Part_1: {part_1_solution(INPUT)}")
+    print(f"Part_2: {part_2_solution(INPUT)}")
